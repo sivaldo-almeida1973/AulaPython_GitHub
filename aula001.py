df7636ac947166f5b8fbf04a28e486d9368d2ffc
Sivaldo Vieira <vieiralmeida.com@hotmail.com>
2023-12-24 20:29:16 -0300

    criada aula001.py

---
+++ aula001.py
@@ -0,0 +1,30 @@
+#permite escrever um comentario
+
+print(123)
+
+#abaixo
+
+print(4567)
+
+
+"""
+Doctstring
+
+"""
+
+'''
+DoctString aspas simples
+Não é um comentario
+
+'''
+
+# \r\n  -> CRLF
+print(12, 34, 48)
+
+print(12,48,55, sep=' ')
+print(12,48,'brasil', sep="/", end='##\n\n')
+print(12,48,55, sep=",")
+print(12,48,55, sep="/", end=' ') # end (quebra de linha)
+print(33,48,55, sep="-")
+print(12,48,55, sep="/")
+print(12,48,55, sep="/", end='')
