27709b6ca81af401a8919fdb3dc515ba1ce5002f
Sivaldo Vieira <vieiralmeida.com@hotmail.com>
2023-12-24 20:43:08 -0300

    criada aula002.py

---
+++ aula002.py
@@ -0,0 +1,25 @@
+"""
+Python - linguagem de programação
+Tipo de tipagem = dinamica / forte
+
+str -> string -> texto
+strings são textos dentro de aspas
+
+"""
+
+print(1234)
+
+#aspas simples
+print('sivaldo vieira')
+
+#aspas duplas
+print("sivaldo vieira")
+
+#escape
+print(" 'siavldo vieira' ")
+print(' "sivaldo vieira" ')
+
+print(r'sivaldo \"vieira" ')
+
+
+
