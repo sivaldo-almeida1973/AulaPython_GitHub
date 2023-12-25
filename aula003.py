824191539eac57697a810984cc85e52694c6ee03
Sivaldo Vieira <vieiralmeida.com@hotmail.com>
2023-12-25 20:26:56 -0300

    criada aula003.py

---
+++ aula003.py
@@ -0,0 +1,35 @@
+"""
+Tipos float e int
+int -> numeros inteiros (positivo ou negativo)
+float -> numeros com ponto flutuante (negativo ou posittivo)
+
+A funação type mostra o tipo que o python inferiu o valor
+"""
+
+#int -> numeros inteiros (positivo ou negativo)
+print(11)
+print(type(11))
+print(-11)
+print(type(-11))
+print(0)
+
+
+#float -> numeros com ponto flutuante (negativo ou posittivo)
+print(1.10)
+print(type(1.10))
+
+print(-1.10)
+print(type(-1.10))
+
+#string
+print(type('siavldo'))
+
+
+#tipo boolean(true ou false)
+#so existem duas respostas possíveis
+
+print(10==10)
+print(type(10 == 10))
+print(10<10)
+print(type(10 < 10))
+
