ac3e478fed95d416fb06c7f7be62180213f7f797
Sivaldo Vieira <vieiralmeida.com@hotmail.com>
2023-12-25 21:06:16 -0300

    criada variaveis005.py.py

---
+++ variaveis005.py
@@ -0,0 +1,36 @@
+"""
+variaveis ->usadas para salvar algo na memoria do computador
+
+Ex:
+nome_variavel = 10
+
+nome_completo = 'Sivadlo vieira de almeida'
+
+print(nome_completo)
+
+nomeVariavel = 11
+
+"""
+
+# nome_completo = 'Sivadlo vieira de almeida'
+
+# print(nome_completo)
+
+
+# soma_numeros = 2 + 2
+# print(soma_numeros)
+
+# num = int('1')
+
+# print(type(num ))
+
+
+nome = 'sivaldo'
+idade = 30
+maior_idade = idade >= 18
+
+
+print('Nome:', nome, 'Idade:', idade)
+
+print('Idade é maior:', maior_idade)
+
