03ddea29a31a857d019e50a5d00e2116e4f2a1b9
Sivaldo Vieira <vieiralmeida.com@hotmail.com>
2023-12-26 10:01:28 -0300

    criado op_aritimeticos.py

---
+++ op_aritimeticos.py
@@ -0,0 +1,35 @@
+adicao = 10 +10
+print('Adicao:', adicao)
+
+subtracao = 10 - 10
+print('Subtração:', subtracao)
+
+multiplicacao = 10 * 10
+print('Multiplicaoa:', multiplicacao)
+
+divisao = 10 / 3
+print('Divisao:', divisao)
+
+divisao_inteiro = 10  // 3
+print('Divisao inteiro:', divisao_inteiro)
+
+potencia = 10 ** 2
+print('Potencia:', potencia)
+
+modulo = 12 %  10
+print('Modulo:', modulo)
+
+
+#================Concatenacao===========================
+
+concatenar = 'Sivaldo' + ' 1 ' +' vieira ' + 'almeida'
+print(concatenar)
+
+
+
+nome = 3 * 'vieira'
+print(nome)
+
+
+num = 100 * 'a'
+print(num)
