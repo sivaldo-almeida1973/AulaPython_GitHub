1503747b04a7329197bcfaa9b9afefe2d8f73b72
Sivaldo Vieira <vieiralmeida.com@hotmail.com>
2023-12-26 10:57:47 -0300

    criado input.py

---
+++ input.py
@@ -0,0 +1,21 @@
+# nome = input('qual seu nome:')
+# print("Nome;" , nome)
+
+# num1 = int(input('Digite um numero:'))
+# num2 = int(input('Digite oureo numero:'))
+
+# soma = num1 + num2
+
+# print("Soma;" ,soma)
+
+
+
+
+
+
+num1 = input('Digite um numero:')
+num2 = input('Digite oureo numero:')
+
+soma = int(num1) + int(num2)
+
+print("Soma;" ,soma)
