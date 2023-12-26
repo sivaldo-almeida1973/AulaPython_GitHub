7b18880f96ab92b94d34770bfbaa198e2ef787e4
Sivaldo Vieira <vieiralmeida.com@hotmail.com>
2023-12-26 09:44:26 -0300

    criado pasta exercicios/ex001.py

---
+++ exercicios/ex001.py
@@ -0,0 +1,14 @@
+nome = 'Sivaldo'
+sobrenome = 'Vieira almeida'
+idade = 35
+ano_nasc = 2023 - idade
+maior_idade = idade >= 18
+altura_metros = 1.70
+
+
+print('Nome:', nome)
+print('Sobrenome:', sobrenome)
+print('Idade:', idade)
+print('Ano Nasc:', ano_nasc)
+print('É maior idade::', maior_idade)
+print('Altura em metros:', altura_metros)
