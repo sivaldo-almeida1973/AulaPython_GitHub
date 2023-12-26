52302e2539dcb545b1fdc7751a42e17942b1fba6
Sivaldo Vieira <vieiralmeida.com@hotmail.com>
2023-12-26 11:16:23 -0300

    criado if_else.py

---
+++ if_else.py
@@ -0,0 +1,58 @@
+# #if / elif   else
+
+
+# entrada = input('Voce quer "entrar" po "sair"? ')
+
+
+# if entrada == 'entrar':
+#  print('Voce entrou no sistema')
+# elif entrada == 'sair':
+#  print('Voce saiu no sistema')
+# else:
+#  print('Opcao invalida')
+
+
+# print('fora do sistema')
+ 
+
+#==============================================
+
+# condicao = True
+
+# if condicao:
+#   print('Este é o codigo do if')
+# else:
+#  print('fora do if')
+
+
+
+
+
+# condicao = False
+
+# if condicao:
+#   print('Este é o codigo do if')
+
+
+# print('fora do if')
+
+
+condicao1 = True
+condicao2 = False
+condicao3 = True
+condicao4 = False
+
+if condicao1:
+  print('dentro da condicao 1')
+elif condicao2:
+  print('dentro da condicao 2')
+elif condicao3:
+  print('dentro da condicao 3')
+elif condicao4:
+  print('dentro da condicao 4')
+
+else:
+  print('condicao invalida')
+
+print('Fora do bloco........')
+
